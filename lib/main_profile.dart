6d2936415e6322e160c915ceec366b2851fef6f4
masaki-abf <kiyuu.dmp@gmail.com>
2026-08-21 18:00:52 +0900

    マイページ確認用エントリーポイントを追加

---
+++ lib/main_profile.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'profile_page.dart';
+
+void main() {
+  runApp(const ProfileApp());
+}
+
+class ProfileApp extends StatelessWidget {
+  const ProfileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ProfilePage(),
+    );
+  }
+}
