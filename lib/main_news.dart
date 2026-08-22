8b58b7b19b0459daec0153a677aabf68bf4e5654
Shuya0127 <e9404@g.maizuru-ct.ac.jp>
2026-08-22 16:53:34 +0900

    firebaseてすと

--- lib/main_news.dart
+++ lib/main_news.dart
@@ -1,10 +1,9 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'config/supabase_config.dart';
 import 'news/news_home_page.dart';
-import 'services/notification_service.dart';
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,9 +12,6 @@ Future<void> main() async {
     url: SupabaseConfig.url,
     publishableKey: SupabaseConfig.publishableKey,
   );
-
-  await Firebase.initializeApp();
-  await NotificationService.initialize();
 
   runApp(const NewsApp());
 }
